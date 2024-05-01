d6348b145a2e0427e4a0cc09c8b97fff229129ae
Nadhifa15 <nadhifa5631@smk.belajar.id>
2024-05-01 14:39:54 +0700

    nadhifa : membenarkan produk

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Product extends Model
 {
-    use HasFactory;
-    
+
+    use HasFactory, SoftDeletes;
+
     protected $casts = [
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
     ];
 
     protected $fillable = [
@@ -23,5 +27,7 @@ class Product extends Model
         'status_publish',
         'slug_link',
     ];
+
+    protected $table = 'products';
 
 }
--- resources/views/Produk/Admin/AdmProduk.blade.php
+++ resources/views/Produk/Admin/AdmProduk.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+
 <div class="row justify-content-center text-center">
 
 
@@ -11,33 +12,32 @@
         </div>
     </div>
 
-
     <div class="col-12">
-        <a href="{{ route('Admin.admin') }}" class="btn btn-success btn-sm">Produk</a> >>
+        <a href="{{ route('Admin.admin') }}" class="btn btn-success btn-sm">Produk</a>
+        <a href="{{ route('Admin.create') }}" class="btn btn-primary m-3 btn-sm"><i class="bi bi-square-plus"></i></i> Tambah</a>
         <a href="{{ route('Admin.history') }}" class="btn btn-dark btn-sm">History</a>
     </div>
-    <div class="col-12 mt-4">
-        <a href="{{ route('Admin.create') }}" class="btn btn-primary btn-sm"><i class="bi bi-square-plus"></i></i> Tambah</a>
-    </div>
 
+    @foreach ($products as $pdk )
+        <div class="col-4">
+            <div class="card my-3">
+                {{-- <div class="container-fluid" style="height: 300px; width: 330px;"> --}}
+                    <img src="{{ asset('storage/images/' . $pdk->foto_product) }}" class="card-img-top" alt="Bolen Jonegoroan {{ $pdk->variant_product }}">
+                {{-- </div> --}}
+                <div class="card-body">
+                    <h4 class="card-title"><strong> {{ $pdk->variant_product }} </strong></h4>
+                    <p class="card-text"> {{ $pdk->description_product }} </p>
+                    <h5> {{ $pdk->harga_product }} </h5>
+                    <p> {{ $pdk->jumlah_product }} </p>
+                    <p> {{ $pdk->status_publish }} </p>
 
-    <div class="col-3">
-        @foreach ($products as $pdk )
-        <div class="card my-3">
-            <img src="{{ asset('storage/product/' . $pdk->foto_product) }}" class="card-img-top" alt="Bolen Jonegoroan">
-            <div class="card-body">
-                <h4 class="card-title"><strong> {{ $pdk->variant_product }} </strong></h4>
-                <p class="card-text"> {{ $pdk->description_product }} </p>
-                <h5> {{ $pdk->harga_product }} </h5>
-                <p> {{ $pdk->jumlah_product }} </p>
-                {{-- <p> {{ $pdk->status_publish }} </p> --}}
-
-                <a href="{{route('Admin.edit', $pdk->slug_link)}}" value="edit" class="btn btn-outline-success mt-2"><i class="bi bi-pencil-square"></i></a>
-                <a href="{{route('Admin.hapus', $pdk->slug_link)}}" value="hapus" class="btn btn-outline-danger mt-2"><i class="bi bi-trash3"></i></a>
+                    <a href="{{route('Admin.edit', $pdk->slug_link)}}" value="edit" class="btn btn-outline-success mt-2"><i class="bi bi-pencil-square"></i></a>
+                    <a href="{{route('Admin.hapus', $pdk->slug_link)}}" value="hapus" class="btn btn-outline-danger mt-2"><i class="bi bi-trash3"></i></a>
+                </div>
             </div>
         </div>
-    </div>
     @endforeach
+
 
 </div>
 
