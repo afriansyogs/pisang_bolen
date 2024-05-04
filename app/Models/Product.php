0a7f2cdbe8c0330e4d0ea3c488104f64d8490dc6
hlgolv6 <helgaolivia1@gmail.com>
2024-05-04 10:44:55 +0700

    2

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Product extends Model
+{
+
+    use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+    protected $fillable = [
+        'foto_product',
+        'variant_product',
+        'description_product',
+        'harga_product',
+        'jumlah_product',
+        'status_publish',
+        'slug_link',
+    ];
+
+    protected $table = 'products';
+
+}
